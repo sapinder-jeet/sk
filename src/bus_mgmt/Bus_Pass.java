8f1eb9dfad9a632d0d1d29f03a5fcc9786ff3784
Sapinderjit Kaur <114123107+sapinderjit-kaur@users.noreply.github.com>
2023-01-29 12:35:01 -0500

    Update Bus_Pass.java

--- src/bus_mgmt/Bus_Pass.java
+++ src/bus_mgmt/Bus_Pass.java
@@ -220,7 +220,7 @@ public class Bus_Pass extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+       
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Bus_Pass().setVisible(true);
